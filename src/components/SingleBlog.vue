e7a12eba0733ebab5419832fb481c71a2d238543
benjun_xu <894012405@qq.com>
2019-02-25 17:39:17 +0800

    个人博客，写博客\编辑\删除

---
+++ src/components/SingleBlog.vue
@@ -0,0 +1,58 @@
+<template>
+	<div id="single-blog">
+		<h1>{{blog.title}}</h1>
+		<article>{{blog.content}}</article>
+		<p>作者： {{blog.author}}</p>
+		<p>分类：</p>
+		<ul>
+			<li v-for="category in blog.categories">{{category}}</li>
+		</ul>
+
+		<button @click='deleteSingleBlog()'>删除</button>
+
+		<router-link :to="'/edit/' + id">编辑</router-link>
+
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default{
+		name:'single-blog',
+		data(){
+			return{
+				id: this.$route.params.id,
+				blog:{}
+			}
+		},
+		created(){
+			this.$http.get('https://my-blog-779b7.firebaseio.com/posts/'+this.id + '.json')
+				.then(function(data){
+					console.log(data);
+
+					return data.json();
+					//this.blog = data.body;
+				})
+				.then(function(data){
+					this.blog = data;
+				})
+		},
+		methods:{
+			deleteSingleBlog(){
+				this.$http.delete("https://my-blog-779b7.firebaseio.com/posts/"+this.id+'.json')
+				.then(response => {
+					this.$router.push({path:'/'})
+				})
+			}
+		}
+	}
+</script>
+
+<style type="text/css" scoped>
+	#single-blog{
+		max-width: 960px;
+		margin: 0 auto;
+		padding: 20px;
+		background: #eee;
+		border: 1px dotted #aaa;
+	}
+</style>
